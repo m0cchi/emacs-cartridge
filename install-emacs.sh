5555255c9281b0ea63bb88bd233fff382c786738
mocchi <boom.boom.planet@gmail.com>
2015-09-26 18:50:09 +0900

    fix install

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export PATH=$OPENSHIFT_EMACS_DIR/bin:$PATH
-alias rm='rm -rf'
 
 # working dir
 mkdir $OPENSHIFT_EMACS_DIR/temp
@@ -10,19 +9,19 @@ cd $OPENSHIFT_EMACS_DIR/temp
 # install autoconf
 curl -OL http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz
 tar xzf autoconf-2.69.tar.gz
-rm autoconf-2.69.tar.gz
+rm -rf autoconf-2.69.tar.gz
 autoconf-2.69/configure --prefix=$OPENSHIFT_EMACS_DIR
 make && make install
-rm *
+rm -rf *
 
 # install emacs
 git clone git://git.savannah.gnu.org/emacs.git
 cd emacs
-rm .git
+rm -rf .git
 ./autogen.sh
-CFLAGS=-static LDFLAGS=-static ./configure --prefix=$OPENSHIFT_EMACS_DIR/ --with-x-toolkit=no --without-toolkit-scroll-bars --without-xaw3d --without-sound --without-pop --without-xpm --without-tiff --without-rsvg --without-gconf --without-gsettings --without-selinux --without-gpm --without-makeinfo --without-x
+./configure --prefix=$OPENSHIFT_EMACS_DIR/ --with-x-toolkit=no --without-toolkit-scroll-bars --without-xaw3d --without-sound --without-pop --without-xpm --without-tiff --without-rsvg --without-gconf --without-gsettings --without-selinux --without-gpm --without-makeinfo --without-x --without-all
 make && make install
 cd ../
 
 # remove working dir
-rm temp
+rm -rf temp
