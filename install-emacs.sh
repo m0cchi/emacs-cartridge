b19c36a7bc14a57469406ac91846ec40e2195ba0
mocchi <boom.boom.planet@gmail.com>
2015-09-27 16:39:13 +0900

    fix install prefix

--- install-emacs.sh
+++ install-emacs.sh
@@ -8,7 +8,7 @@ cd $OPENSHIFT_EMACS_DIR/temp
 wget https://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz
 tar -Jxvf emacs-24.5.tar.xz
 cd emacs-24.5
-./configure --without-x --without-x --prefix=$OPENSHIFT_EMACS_DIR/bin
+./configure --without-x --without-x --prefix=$OPENSHIFT_EMACS_DIR/
 make && make install
 cd ../
 
