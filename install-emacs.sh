95f18650fad3661601413c24ed45d97efc8cb518
mocchi <boom.boom.planet@gmail.com>
2015-09-27 16:28:49 +0900

    fix installer

--- install-emacs.sh
+++ install-emacs.sh
@@ -5,21 +5,10 @@ export PATH=$OPENSHIFT_EMACS_DIR/bin:$PATH
 # working dir
 mkdir $OPENSHIFT_EMACS_DIR/temp
 cd $OPENSHIFT_EMACS_DIR/temp
-
-# install autoconf
-curl -OL http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz
-tar xzf autoconf-2.69.tar.gz
-rm -rf autoconf-2.69.tar.gz
-autoconf-2.69/configure --prefix=$OPENSHIFT_EMACS_DIR
-make && make install
-rm -rf *
-
-# install emacs
-git clone git://git.savannah.gnu.org/emacs.git
-cd emacs
-rm -rf .git
-./autogen.sh
-./configure --prefix=$OPENSHIFT_EMACS_DIR/ --with-x-toolkit=no --without-toolkit-scroll-bars --without-xaw3d --without-sound --without-pop --without-xpm --without-tiff --without-rsvg --without-gconf --without-gsettings --without-selinux --without-gpm --without-makeinfo --without-x --without-all
+wget https://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz
+tar -Jxvf emacs-24.5.tar.xz
+cd emacs-24.5
+./configure --without-x --without-x --prefix=$OPENSHIFT_EMACS_DIR/bin
 make && make install
 cd ../
 
